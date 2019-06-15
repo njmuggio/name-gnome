46787ca75e4fda310a9738ae4450cb3c67f28cd7
Nicholas Muggio <nick@muggio.xyz>
2019-06-14 20:42:34 -0600

    Initial implementation

---
+++ NameGnome/MainForm.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace NameGnome
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.lblGenerated = new System.Windows.Forms.Label();
+            this.btnGenerate = new System.Windows.Forms.Button();
+            this.chkAuto = new System.Windows.Forms.CheckBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lstHistory = new System.Windows.Forms.ListBox();
+            this.txtPattern = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblGenerated
+            // 
+            this.lblGenerated.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblGenerated.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGenerated.Location = new System.Drawing.Point(172, 12);
+            this.lblGenerated.Name = "lblGenerated";
+            this.lblGenerated.Size = new System.Drawing.Size(416, 368);
+            this.lblGenerated.TabIndex = 3;
+            this.lblGenerated.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnGenerate
+            // 
+            this.btnGenerate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnGenerate.Location = new System.Drawing.Point(12, 409);
+            this.btnGenerate.Name = "btnGenerate";
+            this.btnGenerate.Size = new System.Drawing.Size(75, 23);
+            this.btnGenerate.TabIndex = 4;
+            this.btnGenerate.Text = "Generate";
+            this.btnGenerate.UseVisualStyleBackColor = true;
+            this.btnGenerate.Click += new System.EventHandler(this.btnGenerate_Click);
+            // 
+            // chkAuto
+            // 
+            this.chkAuto.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.chkAuto.AutoSize = true;
+            this.chkAuto.Location = new System.Drawing.Point(93, 413);
+            this.chkAuto.Name = "chkAuto";
+            this.chkAuto.Size = new System.Drawing.Size(73, 17);
+            this.chkAuto.TabIndex = 5;
+            this.chkAuto.Text = "Automatic";
+            this.chkAuto.UseVisualStyleBackColor = true;
+            this.chkAuto.CheckedChanged += new System.EventHandler(this.chkAuto_CheckedChanged);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lstHistory
+            // 
+            this.lstHistory.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lstHistory.FormattingEnabled = true;
+            this.lstHistory.Location = new System.Drawing.Point(594, 12);
+            this.lstHistory.Name = "lstHistory";
+            this.lstHistory.Size = new System.Drawing.Size(200, 420);
+            this.lstHistory.TabIndex = 7;
+            this.lstHistory.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.lstHistory_MouseDoubleClick);
+            // 
+            // txtPattern
+            // 
+            this.txtPattern.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtPattern.Location = new System.Drawing.Point(12, 383);
+            this.txtPattern.Name = "txtPattern";
+            this.txtPattern.Size = new System.Drawing.Size(154, 20);
+            this.txtPattern.TabIndex = 8;
+            this.txtPattern.Text = "$C\'$C$v$c $N($n|$a)";
+            this.txtPattern.TextChanged += new System.EventHandler(this.txtPattern_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(12, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 365);
+            this.label1.TabIndex = 9;
+            this.label1.Text = resources.GetString("label1.Text");
+            this.label1.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 444);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPattern);
+            this.Controls.Add(this.lstHistory);
+            this.Controls.Add(this.chkAuto);
+            this.Controls.Add(this.btnGenerate);
+            this.Controls.Add(this.lblGenerated);
+            this.Name = "MainForm";
+            this.Text = "Name Gnome";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblGenerated;
+        private System.Windows.Forms.Button btnGenerate;
+        private System.Windows.Forms.CheckBox chkAuto;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox lstHistory;
+        private System.Windows.Forms.TextBox txtPattern;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ NameGnome/MainForm.cs
@@ -0,0 +1,417 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NameGnome
+{
+    public partial class MainForm : Form
+    {
+        public static List<string> Adjectives { get; private set; }
+        public static List<string> Adverbs { get; private set; }
+        public static List<string> Nouns { get; private set; }
+        public static List<string> Senses { get; private set; }
+        public static List<string> Verbs { get; private set; }
+        public static List<string> Consonants { get; private set; }
+        public static List<string> Vowels { get; private set; }
+        public static Random Random { get; private set; }
+        private Node root;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Random = new Random();
+
+            Consonants = new List<string>
+            {
+                "b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"
+            };
+
+            Vowels = new List<string>
+            {
+                "a", "e", "i", "o", "u"
+            };
+
+            root = Node.Parse(txtPattern.Text);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            Adjectives = getWords("NameGnome.dict.index.adj");
+            Adverbs = getWords("NameGnome.dict.index.adv");
+            Nouns = getWords("NameGnome.dict.index.noun");
+            Senses = getWords("NameGnome.dict.index.sense");
+            Verbs = getWords("NameGnome.dict.index.verb");
+        }
+
+        private List<string> getWords(string resource)
+        {
+            List<string> ret = new List<string>();
+            var assembly = Assembly.GetExecutingAssembly();
+
+            Regex badChars = new Regex("[\\d_]", RegexOptions.Compiled);
+
+            using (var stream = assembly.GetManifestResourceStream(resource))
+            using (var reader = new StreamReader(stream))
+            {
+                while (!reader.EndOfStream)
+                {
+                    string line = reader.ReadLine();
+                    if (line.StartsWith(" "))
+                    {
+                        continue;
+                    }
+
+                    var word = line.Split(new[] {' '}, StringSplitOptions.RemoveEmptyEntries)[0];
+
+                    if (!badChars.IsMatch(word))
+                    {
+                        ret.Add(word);
+                    }
+                }
+            }
+
+            return ret;
+        }
+
+        private void txtPattern_TextChanged(object sender, EventArgs e)
+        {
+            // Attempt to parse pattern
+            root = Node.Parse(txtPattern.Text);
+            if (root == null)
+            {
+                txtPattern.ForeColor = Color.DarkRed;
+            }
+            else
+            {
+                txtPattern.ForeColor = Color.Black;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (chkAuto.Checked && root != null)
+            {
+                Generate();
+            }
+        }
+
+        private void chkAuto_CheckedChanged(object sender, EventArgs e)
+        {
+            timer1.Enabled = chkAuto.Checked;
+        }
+
+        private void btnGenerate_Click(object sender, EventArgs e)
+        {
+            Generate();
+        }
+
+        private void Generate()
+        {
+            if (root != null)
+            {
+                var name = root.Generate();
+                lstHistory.Items.Insert(0, name);
+                lblGenerated.Text = name;
+            }
+        }
+
+        private void lstHistory_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (lstHistory.SelectedItem != null)
+            {
+                Clipboard.SetText(lstHistory.SelectedItem.ToString());
+            }
+        }
+    }
+
+    class Node
+    {
+        private List<Node> children;
+        private Type type;
+        private bool capitalize;
+        private string value;
+
+        public Node()
+        {
+            children = new List<Node>();
+            type = Type.Series;
+            capitalize = false;
+            value = "";
+        }
+
+        public static Node Parse(string pattern)
+        {
+            Node ret = new Node();
+
+            string current = "";
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (pattern[i] == '$')
+                {
+                    if (current != "")
+                    {
+                        Node literal = new Node();
+                        literal.value = current;
+                        literal.type = Type.Literal;
+                        ret.children.Add(literal);
+                    }
+
+                    current = "";
+
+                    Node child = new Node();
+                    i++;
+                    if (i < pattern.Length)
+                    {
+                        switch (pattern[i])
+                        {
+                            case 'd':
+                                child.type = Type.Adverb;
+                                break;
+                            case 'D':
+                                child.type = Type.Adverb;
+                                child.capitalize = true;
+                                break;
+                            case 'a':
+                                child.type = Type.Adjective;
+                                break;
+                            case 'A':
+                                child.type = Type.Adjective;
+                                child.capitalize = true;
+                                break;
+                            case 'n':
+                                child.type = Type.Noun;
+                                break;
+                            case 'N':
+                                child.type = Type.Noun;
+                                child.capitalize = true;
+                                break;
+                            case 's':
+                                child.type = Type.Sense;
+                                break;
+                            case 'S':
+                                child.type = Type.Sense;
+                                child.capitalize = true;
+                                break;
+                            case 'r':
+                                child.type = Type.Verb;
+                                break;
+                            case 'R':
+                                child.type = Type.Verb;
+                                child.capitalize = true;
+                                break;
+                            case 'c':
+                                child.type = Type.Consonant;
+                                break;
+                            case 'C':
+                                child.type = Type.Consonant;
+                                child.capitalize = true;
+                                break;
+                            case 'v':
+                                child.type = Type.Vowel;
+                                break;
+                            case 'V':
+                                child.type = Type.Vowel;
+                                child.capitalize = true;
+                                break;
+                            default:
+                                return null;
+                        }
+
+                        ret.children.Add(child);
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else if (pattern[i] == '\\')
+                {
+                    i++;
+                    if (i < pattern.Length)
+                    {
+                        current += pattern[i];
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else if (pattern[i] == '(')
+                {
+                    if (current != "")
+                    {
+                        Node literal = new Node();
+                        literal.value = current;
+                        literal.type = Type.Literal;
+                        ret.children.Add(literal);
+                    }
+
+                    current = "";
+
+                    int depth = 1;
+                    int end = -1;
+                    for (int j = i + 1; j < pattern.Length; j++)
+                    {
+                        if (pattern[j] == ')' && pattern[j - 1] != '\\')
+                        {
+                            depth--;
+                        }
+                        else if (pattern[j] == '(' && pattern[j - 1] != '\\')
+                        {
+                            depth++;
+                        }
+
+                        if (depth == 0)
+                        {
+                            end = j;
+                            break;
+                        }
+                    }
+
+                    if (end > 0)
+                    {
+                        Node child = Node.Parse(pattern.Substring(i + 1, end - i - 1));
+                        ret.children.Add(child);
+                        i = end;
+                    }
+                    else
+                    {
+                        return null;
+                    }
+                }
+                else if (pattern[i] == '|')
+                {
+                    if (current != "")
+                    {
+                        Node literal = new Node();
+                        literal.value = current;
+                        literal.type = Type.Literal;
+                        ret.children.Add(literal);
+                    }
+
+                    current = "";
+
+                    int prevSeries;
+                    for (prevSeries = ret.children.Count - 1; prevSeries >= 0; prevSeries--)
+                    {
+                        if (ret.children[prevSeries].type == Type.Series)
+                        {
+                            break;
+                        }
+                    }
+
+                    ret.type = Type.Choice;
+
+                    List<Node> seriesChildren = ret.children.Skip(prevSeries + 1).TakeWhile(n => true).ToList();
+                    ret.children.RemoveRange(prevSeries + 1, ret.children.Count - prevSeries - 1);
+                    Node seriesChild = new Node();
+                    seriesChild.type = Type.Series;
+                    seriesChild.children = seriesChildren;
+                    ret.children.Add(seriesChild);
+                }
+                else
+                {
+                    current += pattern[i];
+                }
+            }
+
+            if (current != "")
+            {
+                Node literal = new Node();
+                literal.value = current;
+                literal.type = Type.Literal;
+                ret.children.Add(literal);
+            }
+
+            if (ret.type == Type.Choice)
+            {
+                int prevSeries;
+                for (prevSeries = ret.children.Count - 1; prevSeries >= 0; prevSeries--)
+                {
+                    if (ret.children[prevSeries].type == Type.Series)
+                    {
+                        break;
+                    }
+                }
+
+                ret.type = Type.Choice;
+
+                List<Node> seriesChildren = ret.children.Skip(prevSeries + 1).TakeWhile(n => true).ToList();
+                ret.children.RemoveRange(prevSeries + 1, ret.children.Count - prevSeries - 1);
+                Node seriesChild = new Node();
+                seriesChild.type = Type.Series;
+                seriesChild.children = seriesChildren;
+                ret.children.Add(seriesChild);
+            }
+
+            return ret;
+        }
+
+        public string Generate()
+        {
+            switch (type)
+            {
+                case Type.Literal:
+                    return value;
+                case Type.Adverb:
+                    return Choose(MainForm.Adverbs, capitalize);
+                case Type.Adjective:
+                    return Choose(MainForm.Adjectives, capitalize);
+                case Type.Noun:
+                    return Choose(MainForm.Nouns, capitalize);
+                case Type.Sense:
+                    return Choose(MainForm.Senses, capitalize);
+                case Type.Verb:
+                    return Choose(MainForm.Verbs, capitalize);
+                case Type.Consonant:
+                    return Choose(MainForm.Consonants, capitalize);
+                case Type.Vowel:
+                    return Choose(MainForm.Vowels, capitalize);
+                case Type.Choice:
+                    return children[MainForm.Random.Next(0, children.Count)].Generate();
+                case Type.Series:
+                    string ret = "";
+                    children.ForEach(n => ret += n.Generate());
+                    return ret;
+                default:
+                    return null;
+            }
+        }
+
+        private string Choose(List<string> options, bool capitalize)
+        {
+            var str = options[MainForm.Random.Next(0, options.Count)];
+            if (capitalize)
+            {
+                str = char.ToUpper(str[0]) + str.Substring(1);
+            }
+
+            return str;
+        }
+
+        enum Type
+        {
+            Literal,
+            Adverb,
+            Adjective,
+            Noun,
+            Sense,
+            Verb,
+            Consonant,
+            Vowel,
+            Choice,
+            Series
+        }
+    }
+}
